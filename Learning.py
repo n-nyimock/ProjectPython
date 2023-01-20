3163976384b7f89e9b9fc3db6723f56eb9904f09
n-nyimock <justicennyiimock@gmail.com>
2023-01-20 16:19:18 +0000

    Update Learning.py

--- Learning.py
+++ Learning.py
@@ -1,1 +1,30 @@
-print('Hello Justice')
+x = input("What is your name? ")
+
+
+def function():
+    print(x.upper()+ ", you are Awesome")
+
+function()
+
+
+
+
+#Global variable
+def globe():
+    global x #makes the variable x to be used anywhere
+    x = "Awesome"
+globe()
+print('Python is '+x)
+
+
+
+#Incorrect, will throw an error/global not set
+def lobe():
+    y = "Mind blowing"
+
+lobe()
+print('Python is '+y) #error y is not defined
+
+
+b = 1j
+print(type(b))
